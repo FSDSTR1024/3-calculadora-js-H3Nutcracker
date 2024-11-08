bd2867f3f420a05cfde12dbf425986fde70ec7c4
H3Nutcracker92 <hdulon92@gmail.com>
2024-11-08 02:54:54 +0100

    Añadido algo de funcionalidad a los botones

--- Calculadora.js
+++ Calculadora.js
@@ -1,17 +1,62 @@
-function clearDisplay() {
+// *Funcionamiento del display y los botones de borar* //
+function updateDisplay(value) {
   const display = document.getElementById("display");
-  display.value = "";
+  if (display.textContent.length < 10) {
+    if (display.value === "" && value === "00") {
+      display.value = "0.0";
+    } else if (display.value === "" && value === ".") {
+      display.value = "0.";
+    } else if (display.value === "0" && value === "0") {
+      display.value = "0.0";
+    } else if (display.value === "0" && value === "00") {
+      display.value = "0.00";
+    } else {
+      display.value += value;
+    }
+  } else {
+    display.value = value;
+  }
   updateClearButton();
 }
 
+// **Cambia el botón de limpiar en función de lo que haya en el display** //
 function updateClearButton() {
   const display = document.getElementById("display");
   const clearButton = document.getElementById("clear");
   clearButton.textContent = display.value === "" ? "AC" : "C";
 }
 
-function updateDisplay(value) {
+function clearDisplay() {
   const display = document.getElementById("display");
-  display.value += value;
+  display.value = "";
   updateClearButton();
+}
+
+function erase() {
+  const display = document.getElementById("display");
+  //
+}
+
+function percentage(total, percent) {
+  //
+}
+
+function divide(a, b) {
+  //
+}
+
+function multiply(a, b) {
+  //
+}
+
+function substract(a, b) {
+  //
+}
+
+function add(a, b) {
+  //
+}
+
+function solve() {
+  //
 }
